5cd37ef7ea804788c6168fa0a1131e8c270e1ba4
4thabang <thabang@fordabl.com>
2020-08-09 15:04:02 +0100

    :tada:: Some petty refactoring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Form attributes
-var error = document.getElementById("error");
-var success = document.getElementById("success");
+const error = document.getElementById("error");
+const success = document.getElementById("success");
 const errMess = "This field is required 🥺";
 const succMess = "Calculated Savings 🎉";
 
@@ -9,9 +9,9 @@ error.style.display = "none";
 success.style.display = "none";
 
 // Fees attributes
-var other = document.getElementById("otherfees");
-var fordabl = document.getElementById("fordablfees");
-var savings = document.getElementById("savedfees");
+const other = document.getElementById("otherfees");
+const fordabl = document.getElementById("fordablfees");
+const savings = document.getElementById("savedfees");
 
 // Placeholder
 other.innerHTML = "--";
@@ -19,7 +19,7 @@ fordabl.innerHTML = "--";
 savings.innerHTML = "--";
 
 // Array of selection values to change text
-var aryContent = new Array();
+const aryContent = new Array();
 
 aryContent["0.125"] = "STOCKX FEES";
 aryContent["0.124"] = "GOAT FEES";
@@ -28,20 +28,20 @@ aryContent["0.1290"] = "EBAY FEES";
 aryContent["0.129"] = "DEPOP FEES";
 
 // text value change on selection using array as object for values
-function updateContent(obj) {
-  var othersel = document.getElementById("otherselect");
+const updateContent = (obj) => {
+  const othersel = document.getElementById("otherselect");
   othersel.innerHTML = aryContent[obj[obj.selectedIndex].value];
 }
 
-document.getElementById("formselect").onchange = function() {
+document.getElementById("formselect").onchange = () => {
   updateContent(this);
 };
 
 // Fee calculation function
-function feeCalculation() {
+const feeCalculation = () => {
   // Grab values from HTML
-  let soldAmmt = document.getElementById("form").value;
-  let resellFee = document.getElementById("formselect").value;
+  const soldAmmt = document.getElementById("form").value;
+  const resellFee = document.getElementById("formselect").value;
 
   // Input validation
   if (soldAmmt === "" || resellFee === 0) {
@@ -54,36 +54,43 @@ function feeCalculation() {
   }
 
   // platform fee calculation
-  var val = resellFee * soldAmmt;
+  let val = resellFee * soldAmmt;
 
-  if (resellFee === "0.125") {
-    val = val + 8.5;
-    val = Math.round(val * 100) / 100;
-    val = val.toFixed(2);
-  } else if (resellFee === "0.124") {
-    val = val + 10;
-    val = Math.round(val * 100) / 100;
-    val = val.toFixed(2);
-  } else if (resellFee === "0.1290") {
-    val = val + 0.65;
-    val = Math.round(val * 100) / 100;
-    val = val.toFixed(2);
-  } else if (resellFee === "0.129") {
-    val = val + 0.3;
-    val = Math.round(val * 100) / 100;
-    val = val.toFixed(2);
-  } else if (resellFee === "0.15") {
-    val = Math.round(val * 100) / 100;
-    val = val.toFixed(2);
+  switch (resellFee) {
+    case "0.125":
+      val += 8.5;
+      val = Math.round(val * 100) / 100;
+      val = val.toFixed(2);
+      break;
+    case "0.124":
+      val += 10;
+      val = Math.round(val * 100) / 100;
+      val = val.toFixed(2);
+    case "0.1290":
+      val += 0.65;
+      val = Math.round(val * 100) / 100;
+      val = val.toFixed(2);
+      break;
+    case "0.129":
+      val += 0.3;
+      val = Math.round(val * 100) / 100;
+      val = val.toFixed(2);
+      break;
+    case "0.15":
+      val = Math.round(val * 100) / 100;
+      val = val.toFixed(2);
+      break;
+    default:
+      return;
   }
 
   // fordabl fee calculation
-  var ours = 0.1 * soldAmmt;
+  let ours = 0.1 * soldAmmt;
   ours = Math.round(ours * 100) / 100;
   ours = ours.toFixed(2);
 
   // saved fees calculation
-  var saved = val - ours;
+  let saved = val - ours;
   saved = Math.round(saved * 100) / 100;
   saved = saved.toFixed(2);
 
@@ -93,6 +100,6 @@ function feeCalculation() {
   savings.innerHTML = `£${saved}`;
 }
 
-document.getElementById("button").onclick = function() {
+document.getElementById("button").onclick = () => {
   feeCalculation();
 };
